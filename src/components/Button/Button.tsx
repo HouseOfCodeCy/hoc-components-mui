8e604cca3c978a96c203f03497bf2f6fd597945e
cmario92 <c_mario7@hotmail.com>
2022-12-31 13:50:14 +0200

    testing css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
-import { Button, ButtonProps as MUIButtonProps } from '@mui/material'
+import { Button, ButtonProps as MUIButtonProps, Grid } from '@mui/material'
 import React from 'react'
+import './Button.css'
 import { ButtonProps } from './Button.types'
 
 export interface CustomButtonProps extends ButtonProps, MUIButtonProps {}
 
 const HButton = (props: CustomButtonProps) => {
-  return <Button {...props} />
+  return (
+    <Grid container className='ButtonContainer'>
+      <Button {...props} />)
+    </Grid>
+
 }
 
 export default HButton
