1660ea2d1c5719e93caea656f0d18576cc3e9a2f
cmario92 <c_mario7@hotmail.com>
2022-12-31 13:42:51 +0200

    removing primereact

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import 'primeicons/primeicons.css'
-import 'primereact/resources/primereact.min.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import MyCounter from './components/MyCounter'
 
 import HButton from './components/Button/Button'
