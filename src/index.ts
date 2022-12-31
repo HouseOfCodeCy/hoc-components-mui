6f94ee2600258cb0479bad6128b0af9b6a1e4c69
cmario92 <c_mario7@hotmail.com>
2022-12-31 14:01:00 +0200

    exporting css

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import MyCounter from './components/MyCounter'
-
 import HButton from './components/Button/Button'
+import MyCounter from './components/MyCounter'
+import './styles/theme.css'
+
 export { MyCounter }
 export { HButton }
