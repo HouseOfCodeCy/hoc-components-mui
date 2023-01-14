51f02a10cbabb3ad859acd1332f31763c6127533
cmario92 <c_mario7@hotmail.com>
2023-01-15 01:00:18 +0200

    fixing FavoriteProductComponent

--- src/components/Product/FavoriteProductComponent/FavoriteProductComponent.tsx
+++ src/components/Product/FavoriteProductComponent/FavoriteProductComponent.tsx
@@ -1,10 +1,10 @@
-import { IProduct } from '@houseofcodecy/hoc-utils'
+import { IProductBody } from '@houseofcodecy/hoc-utils'
 import { Card, CardContent, CardMedia, CardProps, Typography } from '@mui/material'
 import React from 'react'
 import { IElementProps } from '../../../interfaces/common'
 
 interface CustomProps {
-  product: IProduct
+  product: IProductBody
 }
 
 export interface CustomProductFavoriteProps extends IElementProps, CardProps, CustomProps {}
@@ -13,13 +13,13 @@ const FavoriteProductComponent = (props: CustomProductFavoriteProps) => {
   const { product } = props
   return (
     <Card sx={{ w: 1, display: 'flex', justifyContent: 'flex-start' }}>
-      <CardMedia sx={{ flexBasis: ' 5rem' }} image={product?.attributes?.mediaUrl} title={product?.attributes?.name} />
+      <CardMedia sx={{ flexBasis: ' 5rem' }} image={product?.mediaUrl} title={product?.name} />
       <CardContent sx={{ w: 1 }}>
         <Typography gutterBottom variant='body2' component='div'>
-          {product?.attributes?.name}
+          {product?.name}
         </Typography>
         <Typography variant='body2' color='text.secondary'>
-          {product?.attributes?.product_sub_categories?.data[0].attributes.name}
+          {product?.product_sub_categories?.data[0].attributes.name}
         </Typography>
       </CardContent>
     </Card>
