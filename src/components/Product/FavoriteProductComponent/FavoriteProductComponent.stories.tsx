7b751359331092b04b3fd40960eca48088e6051c
cmario92 <c_mario7@hotmail.com>
2023-01-15 01:02:39 +0200

    fixing FavoriteProductComponent 2

--- src/components/Product/FavoriteProductComponent/FavoriteProductComponent.stories.tsx
+++ src/components/Product/FavoriteProductComponent/FavoriteProductComponent.stories.tsx
@@ -17,24 +17,21 @@ export const Primary = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   product: {
-    id: '1',
-    attributes: {
-      name: 'Test',
-      description: 'AOsdkaoskd',
-      price: 29.99,
-      mediaUrl:
-        'https://res.cloudinary.com/houseofcodecy/image/upload/v1673334268/pet-shop/dog/dry%20food/purina-pro-plan-large-breed-beef-puppy-food_og1hsx.png',
-      product_sub_categories: {
-        data: [
-          {
-            id: 1,
-            attributes: {
-              name: 'Test Category',
-              products: { data: [] },
-            },
+    name: 'Test',
+    description: 'AOsdkaoskd',
+    price: 29.99,
+    mediaUrl:
+      'https://res.cloudinary.com/houseofcodecy/image/upload/v1673334268/pet-shop/dog/dry%20food/purina-pro-plan-large-breed-beef-puppy-food_og1hsx.png',
+    product_sub_categories: {
+      data: [
+        {
+          id: 1,
+          attributes: {
+            name: 'Test Category',
+            products: { data: [] },
           },
-        ],
-      },
+        },
+      ],
     },
   },
 }
