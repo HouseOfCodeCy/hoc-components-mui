038ef14c87cc50cd13ef09bcf2cbd7c52c2d117e
cmario92 <c_mario7@hotmail.com>
2023-02-16 00:07:10 +0200

    update to ShopConfigurationInterface

--- src/components/common/HeadMeta.tsx
+++ src/components/common/HeadMeta.tsx
@@ -1,22 +1,22 @@
-import { ConfigurationInterface } from '@houseofcodecy/hoc-utils';
+import { ShopConfigurationInterface } from '@houseofcodecy/hoc-utils';
 import React from 'react';
 
 interface Props {
 	Head: ({ children }: { children: React.ReactNode }) => JSX.Element;
-	configuration: ConfigurationInterface;
+	shopConfiguration: ShopConfigurationInterface;
 	title: string;
 	content?: string;
 }
 
-const HeadMeta = ({ Head, configuration, title, content }: Props) => {
+const HeadMeta = ({ Head, shopConfiguration, title, content }: Props) => {
 	return (
 		<Head>
-			<title>{`${configuration.shopFullName} - ${title}`}</title>
+			<title>{`${shopConfiguration.shopFullName} - ${title}`}</title>
 			<meta
 				name='description'
-				content={`${content} - ${configuration.shopDescription}`}
+				content={`${content} - ${shopConfiguration.shopDescription}`}
 			/>
-			<link rel='icon' href={configuration.favicon} />
+			<link rel='icon' href={shopConfiguration.favicon} />
 		</Head>
 	);
 };
